785da9b3afa3912540749cb9ec040532d2f6455c
Moshe <mobgadol@gmail.com>
2022-10-24 21:26:16 -0400

    got everything to work and initiated a header

--- mekor-chaim/src/App.js
+++ mekor-chaim/src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import About from "./components/About";
+// import Donate from "./components/Donate";
+// import Contact from "./components/Contact";
 
 function App() {
   return (
     <Header />
+    // <main>
+    //   <About />
+    //   <Donate />
+    //   <Contact />
+    // </main>
+    // <Footer />
   );
 }
 
--- mekor-chaim/src/components/Header/index.js
+++ mekor-chaim/src/components/Header/index.js
@@ -1,15 +1,16 @@
-import react from "react";
+import React from "react";
+import logo from "../../assets/mekor-chaim-logo.webp";
 
 const Header = () => {
     return (
         <header>
-      <a href="" className="brand-logo">
-        <img src="assets\mekor-chaim-logo.webp" alt="mekor chaim logo"
+      <a href="/" className="brand-logo">
+        <img src={logo} alt="mekor chaim logo"
       /></a>
       <ul className="links-container">
         <li className="nav-links"><a href="#about">About</a></li>
         <li className="nav-links"><a href="#contact">Contact</a></li>
-        <li className="nav-links"><a href="#">Donate</a></li>
+        <li className="nav-links"><a href="/donate">Donate</a></li>
       </ul>
     </header>
     )
